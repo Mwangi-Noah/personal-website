c465485b4c711cccdf4288a2fabcb95edc390020
Noah <noahmwangi@gmail.com>
2023-03-14 12:53:55 +0300

    Finally got the js script to work

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-function myFunction() {
-    var username = document.getElementById("name");
-    var email = document.getElementById("email");
-    var message = document.getElementById("message");
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+function displayBlock(){
+  modal.style.display = "block";
+}
+
+function displayNone(){
+  modal.style.display = "none";
+}
+
+btn.addEventListener("click", displayBlock);
+span.addEventListener("click", displayNone);
+window.addEventListener("click", function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+})
